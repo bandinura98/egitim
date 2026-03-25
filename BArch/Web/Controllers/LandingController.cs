f3783030b70543fb058c20b1fd8d3ebad26ca1d1
BATUHAN ŞENOĞLU <batuhansenogluis@gmail.com>
2026-03-25 22:02:15 +0300

    ew

---
+++ BArch/Web/Controllers/LandingController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+public class LandingController(IMediator _mediator) : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        /*
+         Burayı sor eğer ki bir kullanıcı giriş yapmışsa ona özel bir sayfa göstermek istiyorsak nasıl olacak.
+         Bearer tokeni nası sokçağız, kullanıcı bilgilerini nasıl alacağız, ona göre nasıl bir sayfa göstereceğiz.
+         */
+        return View();
+    }
+}
