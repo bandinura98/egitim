5926c9392907ec5123a1df69793934aa9da5fd3d
BATUHAN ŞENOĞLU <batuhansenogluis@gmail.com>
2026-03-26 16:50:55 +0300

    fsd

---
+++ BArch/Application/Extensions/ExceptionExtension.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Extensions;
+
+public static class ExceptionExtension
+{
+    private static IEnumerable<Exception> Flatten(this Exception ex)
+    {
+        yield return ex;
+        if (ex.InnerException != null)
+        {
+            foreach (var inner in ex.InnerException.Flatten())
+                yield return inner;
+        }
+    }
+    public static string[] ToStringArray(this Exception ex) {
+        string[] errorArray = ex.Flatten().Select(e => e.Message).ToList().ToArray();
+
+        return errorArray;
+    }
+}
--- BArch/Web/Controllers/LandingController.cs
+++ BArch/Web/Controllers/LandingController.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Application.Features.Users.Commands.Create;
+using Application.Features.Users.Queries.GetById;
+using Domain.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Web.Controllers;
@@ -12,6 +15,36 @@ public class LandingController(IMediator _mediator) : Controller
          Burayı sor eğer ki bir kullanıcı giriş yapmışsa ona özel bir sayfa göstermek istiyorsak nasıl olacak.
          Bearer tokeni nası sokçağız, kullanıcı bilgilerini nasıl alacağız, ona göre nasıl bir sayfa göstereceğiz.
          */
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> SignIn(User user)
+    {
+        var response = await _mediator.Send(new CreateUserCommand(user));
+
+        if (response._IsSuccess)
+        {
+            //ViewBag.UserId = UserId;
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Errors = response._Error!;
+
+        return View();
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> LoginUser(User item)
+    {
+        var response = await _mediator.Send(new GetByIdQuery(item.Id));
+
+        if (response._IsSuccess)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        ViewBag.Errors = response._Error!;
+
         return View();
     }
 }
